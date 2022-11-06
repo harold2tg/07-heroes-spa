223dd440d5f1e294cc1eade3a45b7bd307725b42
Harold Torres Gallo <harold2tg@gmail.com>
2022-11-05 21:43:31 -0500

    189. Colocar clase de la ruta activa y organizando las importaciones

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import {Navbar} from '../ui/components'
 import { LoginPage } from "../auth/pages/LoginPage";
-import { DCPage } from "../heroes/pages/DCPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { DCPage, MarvelPage } from "../heroes";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar/>
         <Routes>
             <Route path="marvel" element={ <MarvelPage/> }>Marvel</Route>
             <Route path="dc" element= { <DCPage/> }>About</Route>
             <Route path="login" element= { <LoginPage/> }>About</Route>
-
             <Route path="/" element={ <Navigate to="/marvel"/> }>Home</Route>
-
         </Routes>
     </>
   )
